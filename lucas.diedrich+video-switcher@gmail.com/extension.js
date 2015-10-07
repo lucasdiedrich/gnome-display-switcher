bbe81c6bd671c2ae23bc37317df198c8b971057d
Lucas Guilherme Diedrich <lucas.diedrich@gmail.com>
2015-10-06 22:26:16 -0300

    Add Gnome-shell 3.14 support and better code

--- lucas.diedrich+video-switcher@gmail.com/extension.js
+++ lucas.diedrich+video-switcher@gmail.com/extension.js
@@ -1,28 +1,36 @@
 
+/*
+  IMPORTS - REALLY LOTS OF THEM!
+*/
 const St = imports.gi.St;
 const Main = imports.ui.main;
-const Tweener = imports.ui.tweener;
 
 const Lang = imports.lang;
-const Signals = imports.signals;
 const Mainloop = imports.mainloop;
 
 const Gio = imports.gi.Gio;
-const Gettext = imports.gettext;
 const Meta = imports.gi.Meta;
 const Shell = imports.gi.Shell;
 
 const Config = imports.misc.config;
 const ExtensionUtils = imports.misc.extensionUtils;
+
 const Me = ExtensionUtils.getCurrentExtension();
-const ShellVersion = Config.PACKAGE_VERSION;
+
+/*
+  Control variables
+*/
+const ShellVersion = Config.PACKAGE_VERSION.split('.');;
 
 const _schema_file = "org.gnome.shell.extensions.video-switcher";
 const _shortcut = "shortcut-switch";
-const _meta_mode = Meta.KeyBindingFlags.NONE;
+const _meta_flags = Meta.KeyBindingFlags.NONE;
 
-let button, _settings, _binding_mode;
+let _button, _settings, _binding_mode;
 
+/*
+  Code Begin
+*/
 function _showMessage(_text) {
  
     let text = _text + " ";
@@ -34,7 +42,7 @@ function _showMessage(_text) {
     label.set_position(Math.floor (monitor.width / 2 - label.width / 2), 
                         Math.floor(monitor.height / 2 - label.height / 2));
     
-    Mainloop.timeout_add(3000, function () { 
+    Mainloop.timeout_add(2000, function () { 
       label.destroy(); 
     });
 }
@@ -57,33 +65,31 @@ function loadSettings() {
     let schemaObj = schemaSource.lookup(_schema_file, true);
     
 
-    if (!schemaObj)
+    if (!schemaObj) {
         throw new Error('Schema ' + _schema_file + ' could not be found for extension '
-                        + Me.metadata.uuid + '. Please check your installation.');
+                          + Me.metadata.uuid + '. Please check your installation.');
+    }
 
     _settings = new Gio.Settings({ settings_schema: schemaObj });
-    //_binding_mode = ShellVersion Shell.KeyBindingMode.NORMAL : Shell.ActionMode.NORMAL
-    _binding_mode = Shell.ActionMode.NORMAL;
+    _binding_mode = ShellVersion[1] <= 14 ? Shell.KeyBindingMode.NORMAL : Shell.ActionMode.NORMAL
 }
 
-function loadKeyBindings(){
+function loadKeyBinding(){
     Main.wm.addKeybinding(
           _shortcut,
           _settings,
-          _meta_mode,
+          _meta_flags,
           _binding_mode,
           function(){
             _showMessage("Hello world from keybind.");
           }
     );
-
-    _showMessage("Keybing loaded");
 }
 
 function init() {
     log ("Loading everything");
 
-    button = new St.Bin({ style_class: 'panel-button',
+    _button = new St.Bin({ style_class: 'panel-button',
                           reactive: true,
                           can_focus: true,
                           x_fill: true,
@@ -92,21 +98,21 @@ function init() {
     let icon = new St.Icon({ icon_name: 'system-run-symbolic',
                              style_class: 'system-status-icon' });
 
-    button.set_child(icon);
+    _button.set_child(icon);
 
-    button.connect('button-press-event', function(){
+    _button.connect('button-press-event', function(){
       _showMessage("Hello World");
     });
 }
 
 function enable() {
-    Main.panel._rightBox.insert_child_at_index(button, 0);
+    Main.panel._rightBox.insert_child_at_index(_button, 0);
     loadSettings();
-    loadKeyBindings();
+    loadKeyBinding();
     log ("Everything has been loaded succesfully.");
 }
 
 function disable() {
-    Main.panel._rightBox.remove_child(button);
+    Main.panel._rightBox.remove_child(_button);
     log ("Everything has been unloaded succesfully.");
 }
